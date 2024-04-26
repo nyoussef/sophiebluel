dedf2a404b9c64526bfb6eaa803e259000845e38
Youssef Nazeer <youssefnazeer@gmail.com>
2024-04-26 17:42:16 +0200

    Erreur

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,6 @@
+let works = [];
+let categories = [];
+
 async function getWorks() {
     const result = await fetch('http://localhost:5678/api/works');
     return result.json();    
@@ -7,6 +10,7 @@ const gallery= document.querySelector('.gallery');
 const filters = document.querySelector('.filters');
 
 function displayWorks(workList) {
+    works = workList;
     for (let index = 0; index < workList.length; index++) {
         const figure = document.createElement('figure');
         const img = document.createElement('img');
@@ -26,34 +30,39 @@ async function getCategories() {
 }
 
 async function displayCategories() {
-    const categoriesList = await getCategories();
+    let categoriesList = await getCategories();
+    categories = categoriesList;
+    categoriesList.push({name:"Tous", id:0})
+    categoriesList.sort((a,b) => a.id - b.id);
     categoriesList.forEach(category => {
         const button = document.createElement('button');
         button.classList.add('filter-button');
         button.id = category.id;
         button.textContent = category.name;
+        button.addEventListener("click", async () => {
+            const workList = await getWorks();
+            gallery.innerHTML = "";
+            //console.log(category.id);
+            if(this.id === 0){
+                displayWorks(workList);
+            }
+            else{
+                displayWorks(workList.filter(p=> p.categoryId==category.id));
+            }
+            this.classList.add('active-filter');
+        })
         filters.appendChild(button);
     });
     
     
-    const filterButtons = document.querySelectorAll('.filter-button');
+    /*const filterButtons = document.querySelectorAll('.filter-button');
     filterButtons.forEach(button => {
         button.addEventListener('click', async function() {
-            filterButtons.forEach(button => {
-                button.classList.remove('active-filter');
-                gallery.innerHTML = "";
-            });
-            const workList = await getWorks();
-            if(this.id === ""){
-                displayWorks(workList);
-            }
-            else{
-                displayWorks(workList.filter(p=> p.categoryId==this.id));
-            }
-            this.classList.add('active-filter');
+            
+            
             console.log(this.id);
         });
-    });
+    });*/
     
 };
 
