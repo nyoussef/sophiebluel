c5ae742f3b2f691bef1787b3d17f54ae02c3d5b7
Youssef Nazeer <youssefnazeer@gmail.com>
2024-05-31 11:43:27 +0200

    Ajout de commentaires détaillés + modification du README

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,91 +1,112 @@
+// Initialisation des variables pour stocker les travaux et les catégories
 let works = [];
 let categories = [];
+
+// Sélection des éléments du DOM nécessaires pour l'application
 const body = document.querySelector("body");
 const projectTitle = document.querySelector("#projectTitle");
 const header = document.querySelector("header");
 const authButton = document.querySelector(".auth_button");
-const token = localStorage.getItem("token");
+const token = localStorage.getItem("token"); // Récupération du token de connexion depuis le localStorage
 const gallery = document.querySelector(".gallery");
 const filters = document.querySelector(".filters");
-//Modal
+
+// Création des éléments du modal
 const modalContainer = document.createElement("div");
-modalContainer.className = "modalContainer display-none";
+modalContainer.className = "modalContainer display-none"; // Conteneur principal du modal, caché par défaut
 const modalContent = document.createElement("div");
-modalContent.className = "modalContent";
+modalContent.className = "modalContent"; // Conteneur du contenu du modal
 const xmark = document.createElement("i");
-xmark.className = "fa-solid fa-xmark";
-const modalTitle = document.createElement("h2");
+xmark.className = "fa-solid fa-xmark"; // Icône de fermeture du modal
+const modalTitle = document.createElement("h2"); // Titre du modal
 const modalGallery = document.createElement("div");
-modalGallery.className = "modalGallery display-flex";
+modalGallery.className = "modalGallery display-flex"; // Galerie des travaux dans le modal
 const addButton = document.createElement("button");
-addButton.className = "addButton hoverButton display-flex";
+addButton.className = "addButton hoverButton display-flex"; // Bouton pour ajouter une photo, affiché par défaut
 addButton.innerHTML = "Ajouter une photo";
-//Modal Ajout
+
+// Création des éléments pour le modal d'ajout
 const arrowLeft = document.createElement("i");
-arrowLeft.className = "fa-solid fa-arrow-left display-none";
+arrowLeft.className = "fa-solid fa-arrow-left display-none"; // Icône pour revenir à la galerie du modal, caché par défaut
 const modalForm = document.createElement("form");
 modalForm.setAttribute("method", "post");
-modalForm.setAttribute("enctype", "multipart/form-data");
-modalForm.className = "display-none";
+modalForm.setAttribute("enctype", "multipart/form-data"); // Formulaire pour ajouter un travail
+modalForm.className = "display-none"; // Formulaire caché par défaut
 const formContent = document.createElement("div");
-formContent.className = "formContent";
+formContent.className = "formContent"; // Conteneur pour le contenu du formulaire
+
+// Création des éléments pour le téléchargement de fichiers
 const fileContainer = document.createElement("div");
-fileContainer.className = "fileContainer";
+fileContainer.className = "fileContainer"; // Conteneur pour les éléments de téléchargement de fichiers
 const fileIcon = document.createElement("i");
-fileIcon.className = "fa-regular fa-image";
+fileIcon.className = "fa-regular fa-image"; // Icône représentant une image
 const fileLabel = document.createElement("label");
 fileLabel.setAttribute("for", "file");
 fileLabel.className = "display-flex";
-fileLabel.innerHTML = "+ Ajouter photo";
+fileLabel.innerHTML = "+ Ajouter photo"; // Étiquette pour le bouton de téléchargement de fichiers
 const fileInput = document.createElement("input");
 fileInput.type = "file";
 fileInput.id = "file";
-fileInput.name = "image";
+fileInput.name = "image"; // Champ d'entrée pour le téléchargement de fichiers
 const fileParagraph = document.createElement("p");
-fileParagraph.innerHTML = "jpg, png : 4mo max";
+fileParagraph.innerHTML = "jpg, png : 4mo max"; // Paragraphe avec des instructions pour le téléchargement de fichiers
 const fileImage = document.createElement("img");
 fileImage.src = "";
-fileImage.alt = "";
+fileImage.alt = ""; // Image de prévisualisation du fichier téléchargé
+
+// Création des éléments pour le titre du travail
 const titleLabel = document.createElement("label");
 titleLabel.setAttribute("for", "title");
-titleLabel.innerHTML = "Titre";
+titleLabel.innerHTML = "Titre"; // Étiquette pour le champ du titre
 const formTitle = document.createElement("input");
 formTitle.type = "text";
 formTitle.id = "title";
-formTitle.name = "title";
+formTitle.name = "title"; // Champ d'entrée pour le titre du travail
+
+// Création des éléments pour la catégorie du travail
 const categoriesLabel = document.createElement("label");
 categoriesLabel.setAttribute("for", "category");
-categoriesLabel.innerHTML = "Catégorie";
+categoriesLabel.innerHTML = "Catégorie"; // Étiquette pour le champ de sélection des catégories
 const formCategories = document.createElement("select");
 formCategories.name = "category";
-formCategories.id = "category";
+formCategories.id = "category"; // Champ de sélection pour les catégories
 const defaultCategoryOption = document.createElement("option");
 defaultCategoryOption.value = "";
 defaultCategoryOption.text = "Sélectionnez une catégorie";
 defaultCategoryOption.disabled = true;
-defaultCategoryOption.selected = true;
+defaultCategoryOption.selected = true; // Option par défaut pour la sélection des catégories
+
+// Création du bouton de soumission du formulaire
 const formButton = document.createElement("button");
 formButton.className = "formButton";
 formButton.innerHTML = "Valider";
-formButton.setAttribute("disabled", "disabled");
+formButton.setAttribute("disabled", "disabled"); // Bouton désactivé par défaut
 
+
+// Fonction principale exécutée lorsque le script est chargé
 async function main() {
+  // Récupération des travaux et des catégories
   works = await getWorks();
   categories = await getCategories();
+  
+  // Si aucun token n'est présent, afficher les catégories
   if (!token) {
     await displayCategories();
   } else {
+    // Sinon, afficher les travaux, activer le mode édition et les autres fonctionnalités
     await displayWorks(works);
     editionMode();
     closeModal();
     checkInputsFilled(); 
-    //Retour Modal Galerie
+    
+    // Gestionnaire d'événement pour revenir à la galerie depuis le modal
     arrowLeft.addEventListener("click", () => {
       clearModal();
       clearAdd();
       displayModalWorks(works);
     });
-    //Modal Ajout
+    
+    // Gestionnaire d'événement pour ouvrir le modal d'ajout
     addButton.addEventListener("click", () => {
       displayModalCategories();
       toggleDisplay(arrowLeft, "flex");
@@ -94,7 +115,8 @@ async function main() {
       toggleDisplay(addButton, "none");
       toggleDisplay(modalForm, "flex");
     });
-    //Préchargement image
+    
+    // Prévisualisation de l'image à ajouter
     previewImage();
     fileImage.addEventListener("click", () => {
       fileInput.click();
@@ -103,18 +125,22 @@ async function main() {
   }
 }
 
+// Appel de la fonction principale
 main();
 
+// Fonction pour récupérer les travaux depuis l'API
 async function getWorks() {
   const result = await fetch("http://localhost:5678/api/works");
   return result.json();
 }
 
+// Fonction pour récupérer les catégories depuis l'API
 async function getCategories() {
   const result = await fetch("http://localhost:5678/api/categories");
   return result.json();
 }
 
+// Fonction pour afficher les travaux dans la galerie
 async function displayWorks(workList) {
   gallery.innerHTML = "";
   for (let index = 0; index < workList.length; index++) {
@@ -130,6 +156,7 @@ async function displayWorks(workList) {
   }
 }
 
+// Fonction pour afficher les catégories sous forme de boutons de filtre
 async function displayCategories() {
   categories.push({ name: "Tous", id: "" });
   categories.sort((a, b) => a.id - b.id);
@@ -147,6 +174,7 @@ async function displayCategories() {
   });
 }
 
+// Fonction pour afficher les travaux filtrés par catégorie
 async function displayWorksByCategories() {
   clearActiveFilter();
   if (this.id == "") {
@@ -157,6 +185,7 @@ async function displayWorksByCategories() {
   this.classList.add("active-filter");
 }
 
+// Fonction pour supprimer le filtre actif
 function clearActiveFilter() {
   const filterButtons = document.querySelectorAll(".filter-button");
   filterButtons.forEach((button) => {
@@ -165,8 +194,9 @@ function clearActiveFilter() {
   gallery.innerHTML = "";
 }
 
+// Fonction pour activer le mode édition (affichage d'éléments supplémentaires)
 function editionMode() {
-  //Modification bouton de connexion
+  // Modification du bouton de connexion pour afficher un bouton de déconnexion
   authButton.innerHTML = "";
   const logoutButton = document.createElement("div");
   logoutButton.innerHTML = "logout";
@@ -176,7 +206,8 @@ function editionMode() {
   };
   logoutButton.addEventListener("click", logout);
   authButton.appendChild(logoutButton);
-  //Création topbar
+  
+  // Création et affichage de la barre supérieure en mode édition
   const topBar = document.createElement("div");
   const topBarText = document.createElement("span");
   topBarText.className = "topBarText";
@@ -185,12 +216,14 @@ function editionMode() {
   header.classList.add("mt20");
   topBar.appendChild(topBarText);
   body.appendChild(topBar);
-  //Création bouton modifier
+  
+  // Création et affichage du bouton modifier
   const editButton = document.createElement("button");
   editButton.className = "editButton";
   editButton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> modifier';
   projectTitle.appendChild(editButton);
-  //Ouverture modal
+  
+  // Gestionnaire d'événement pour ouvrir le modal de modification
   editButton.addEventListener("click", () => {
     toggleDisplay(modalContainer, "flex");
     displayModalWorks(works);
@@ -204,6 +237,7 @@ function editionMode() {
   });
 }
 
+// Fonction pour afficher les travaux dans le modal
 async function displayModalWorks(workList) {
   modalTitle.innerHTML = "Galerie photo";
   modalGallery.innerHTML = "";
@@ -222,15 +256,16 @@ async function displayModalWorks(workList) {
   }
 }
 
+// Fonction pour supprimer un travail via l'API
 async function deleteWork(id) {
-    const init = {
-      method: "DELETE",
-      headers: {
-        accept: "*/*",
-        Authorization: "Bearer " + token,
-      },
-    };
-    fetch("http://localhost:5678/api/works/" + id, init)
+  const init = {
+    method: "DELETE",
+    headers: {
+      accept: "*/*",
+      Authorization: "Bearer " + token,
+    },
+  };
+  fetch("http://localhost:5678/api/works/" + id, init)
     .then((response) => {
       if (!response.ok) {
         return alert("Erreur lors de la suppression");
@@ -244,15 +279,15 @@ async function deleteWork(id) {
     });
 }
 
-
+// Fonction pour ajouter plusieurs enfants à un élément parent
 function appendChildren(parent, ...children) {
   children.forEach((child) => {
     parent.appendChild(child);
   });
 }
 
+// Fonction pour gérer la fermeture du modal
 function closeModal() {
-  //Fermeture modal
   xmark.addEventListener("click", () => {
     toggleDisplay(modalContainer, "none");
     clearModal();
@@ -265,6 +300,7 @@ function closeModal() {
   });
 }
 
+// Fonction pour réinitialiser le contenu du modal
 function clearModal() {
   toggleDisplay(arrowLeft, "none");
   toggleDisplay(modalGallery, "flex");
@@ -274,6 +310,7 @@ function clearModal() {
   clearAdd();
 }
 
+// Fonction pour afficher les catégories dans le formulaire du modal
 async function displayModalCategories() {
   categories.forEach((category) => {
     const option = document.createElement("option");
@@ -283,6 +320,7 @@ async function displayModalCategories() {
   });
 }
 
+// Fonction pour vérifier que tous les champs du formulaire sont remplis
 function checkInputsFilled() {
   modalForm.addEventListener("input", () => {
     if (fileInput.value !== "" && formTitle.value !== "" && formCategories.value !== "") {
@@ -297,6 +335,7 @@ function checkInputsFilled() {
   });
 }
 
+// Fonction pour réinitialiser les champs du formulaire d'ajout
 function clearAdd() {
   toggleDisplay(fileImage, "none");
   fileImage.src = "";
@@ -311,6 +350,7 @@ function clearAdd() {
   clearCategoryList();
 }
 
+// Fonction pour réinitialiser la liste des catégories du formulaire d'ajout
 function clearCategoryList() {
   const removeOptions = document.querySelectorAll('#category option:not([value=""])');
   removeOptions.forEach(option => {
@@ -318,7 +358,7 @@ function clearCategoryList() {
   });
 }
 
-////////// Ajout Work \\\\\\\\\\
+// Gestionnaire d'événement pour l'ajout d'un nouveau travail
 modalForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   if (!fileInput.files[0] || !formTitle.value || !formCategories.value) {
@@ -342,6 +382,7 @@ modalForm.addEventListener("submit", async (e) => {
     });
 });
 
+// Fonction pour prévisualiser l'image avant l'ajout
 function previewImage() {
   fileInput.addEventListener("change", () => {
     const file = fileInput.files[0];
@@ -360,6 +401,7 @@ function previewImage() {
   });
 }
 
+// Fonction pour basculer l'affichage des éléments
 function toggleDisplay(element, displayMode) {
   if (displayMode === "flex") {
     element.classList.add("display-flex");
@@ -370,6 +412,7 @@ function toggleDisplay(element, displayMode) {
   }
 }
 
+// Fonction pour changer la couleur de fond d'un élément en fonction de son état
 function changeBackgroundColor(element, state) {
   if (state === "enabled") {
     element.classList.add("bg-color-enabled");
